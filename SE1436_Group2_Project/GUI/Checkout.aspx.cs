29fef3862d8b634a6936eb7672b177e72d91b3fc
xuannquy <nguyenc501@gmail.com>
2021-07-23 16:16:21 +0700

    fix

--- SE1436_Group2_Project/GUI/Checkout.aspx.cs
+++ SE1436_Group2_Project/GUI/Checkout.aspx.cs
@@ -66,7 +66,7 @@ namespace SE1436_Group2_Project.GUI
                 TableCell cell = new TableCell();
                 HyperLink hyl = new HyperLink();
                 hyl.Text = "Check out";
-                hyl.NavigateUrl = "checkoutroom.aspx?id=" + e.Row.Cells[0].Text + "&room=" + e.Row.Cells[1];
+                hyl.NavigateUrl = "checkoutroom.aspx?id=" + e.Row.Cells[0].Text + "&room=" + e.Row.Cells[1].Text;
                 cell.Controls.Add(hyl);
                 obj.Add(cell);
             }
@@ -77,7 +77,7 @@ namespace SE1436_Group2_Project.GUI
                 TableCell cell = new TableCell();
                 HyperLink hyl = new HyperLink();
                 hyl.Text = "Check out";
-                hyl.NavigateUrl = "checkoutroomoff.aspx?id=" + e.Row.Cells[0].Text + "&room=" + e.Row.Cells[1];
+                hyl.NavigateUrl = "checkoutroomoff.aspx?id=" + e.Row.Cells[0].Text + "&room=" + e.Row.Cells[1].Text;
                 cell.Controls.Add(hyl);
                 obj.Add(cell);
             }
